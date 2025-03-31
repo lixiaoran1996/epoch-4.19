a1941fac3d597b81f6662207dba24be737fcc10b
lixiaoran <lixiaoran1996@foxmail.com>
2025-03-31 16:46:50 +0800

    add super-gaussian option

--- epoch1d/src/user_interaction/ic_module.f90
+++ epoch1d/src/user_interaction/ic_module.f90
@@ -17,6 +17,7 @@ MODULE ic_module
 
   USE shared_data
   USE helper
+  USE numerics
 
   IMPLICIT NONE
 
@@ -26,8 +27,41 @@ MODULE ic_module
 
 CONTAINS
 
-  SUBROUTINE manual_load
 
+  SUBROUTINE manual_load
+    ! TYPE(particle), POINTER :: current
+    ! INTEGER, PARAMETER :: np_local = 1000
+    ! INTEGER :: ispecies, ip
+    ! REAL(num) :: temperature, pte2, p_ref, min_p, max_p, dp_local, p_scale
+    ! REAL(num), DIMENSION(np_local) :: p_axis, distfn_axis
+    ! REAL(num), PARAMETER :: m = 3.0_num  ! 设置m为超高斯分布的指数
+    
+    ! DO ispecies = 1, n_species
+    !   IF (species_list(ispecies)%name == 'electron') THEN
+    !     print *,'found electron'
+    !     temperature = 3000_num  ! 设置温度 eV
+    !     pte2 = q0 * temperature * species_list(ispecies)%mass  ! 计算标准差的平方
+    !     p_ref= SQRT(3.0*pte2*gamma_fn(3.0/m)/gamma_fn(5.0/m))
+
+    !     max_p = 5.0_num*SQRT(pte2)  ! 速度最大值
+    !     min_p = -max_p  ! 速度最小值
+    !     dp_local = (max_p - min_p) / REAL(np_local-1, num)  ! 速度步长
+    !     ! 生成超高斯分布
+    !     DO ip = 1, np_local
+    !         p_axis(ip) = min_p + (ip - 1) * dp_local  ! 计算每个速度点
+    !         p_scale = ABS(p_axis(ip)/p_ref)
+    !         distfn_axis(ip) = EXP(-p_scale ** m)
+    !     ENDDO
+        
+    !     ! 分配粒子的速度
+    !     current => species_list(ispecies)%attached_list%head
+    !     DO WHILE (ASSOCIATED(current))
+    !         current%part_p(1) = sample_dist_function(p_axis, distfn_axis)  ! 从分布中采样速度
+    !         current => current%next
+    !     ENDDO
+    !   ENDIF
+    ! ENDDO
   END SUBROUTINE manual_load
+    
 
 END MODULE ic_module
